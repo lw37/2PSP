176ca6fe4e4411345cfcdedf785ea9951209183d
Wei Luo <luo.luo.ll14@gmail.com>
2020-10-29 16:54:35 +0100

    Creado los fichero

---
+++ src/main/java/MenberMonitor.java
@@ -0,0 +1,7 @@
+public class MenberMonitor {
+    public static void main(String[] args) {
+        Menber menber=new Menber();
+        CorreoElectionico CorreoElec=new CorreoElectionico();
+    }
+}
+
