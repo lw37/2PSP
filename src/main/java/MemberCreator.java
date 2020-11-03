b4326fa4182e2f9d7926d7b8dee665130ea37edc
Wei Luo <luo.luo.ll14@gmail.com>
2020-11-03 12:21:01 +0100

    Funciona crear fichero con lo de anterior

--- src/main/java/MemberCreator.java
+++ src/main/java/MemberCreator.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,13 +13,22 @@ public class MemberCreator implements Runnable {
     @Override
     public synchronized void run() {
         System.out.println("MemberCreator Started");
-
+        File file = new File("correos.txt");
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter printer = new PrintWriter(writer);
         for (int i = 1; i < 1000000000; i++) {
             try {
                 Thread.sleep(4000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            printer.println(i+"@qq.com ");
+            printer.flush();
             correos.add(i+"@qq.com");
             System.out.println("Creado user: "+i+"@qq.com");
         }
@@ -30,7 +43,7 @@ public class MemberCreator implements Runnable {
 
     public void Informar(int num){
         lock.lock();
-            System.out.println("Estimado "+correos.get(num)+", usuario "+correos.getLast()+" ha unido a nuestro grupo.");
+        System.out.println("Estimado "+correos.get(num)+", usuario "+correos.getLast()+" ha unido a nuestro grupo.");
         lock.unlock();
     }
 }
