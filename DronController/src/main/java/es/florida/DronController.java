b7d9d7647657a3680528642065bb038587c4b91c
Wei Luo <luo.luo.ll14@gmail.com>
2021-01-28 19:56:59 +0100

    Act2 hecho

--- DronController/src/main/java/es/florida/DronController.java
+++ DronController/src/main/java/es/florida/DronController.java
@@ -1,31 +1,56 @@
 package es.florida;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class DronController {
 
-    public Socket connect() {
-        // FIXME please implement solution or remove this method
-        return new Socket();
+    private static final int PORT_HTTP = 9876;
+    public DronController (){
+
+    }
+    public Socket connect() throws IOException {
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("localhost", PORT_HTTP));
+        return socket;
     }
 
-    public void takeOff() {
+    public void takeOff(Socket socket ) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter printer = new PrintWriter(new OutputStreamWriter(outputStream), true);
+        printer.println("TAKE-OFF");
         System.out.println("Taking off...");
     }
 
-    public void land() {
+    public void land(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter printer = new PrintWriter(new OutputStreamWriter(outputStream), true);
         System.out.println("Landing");
     }
 
-    public void firePrimaryCannon() {
+    public void firePrimaryCannon(Socket socket) throws  IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter printer = new PrintWriter(new OutputStreamWriter(outputStream), true);
+        printer.println("FIRE_P_W");
         System.out.println("Ratatatatatatata!");
+
     }
 
-    public void fireSecondaryWeapon() {
+    public void fireSecondaryWeapon(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter printer = new PrintWriter(new OutputStreamWriter(outputStream), true);
+        printer.println("FIRE_P_W");
         System.out.println("Piñau! Piñau!");
     }
 
-    public void shutDown() {
+    public void shutDown(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter printer = new PrintWriter(new OutputStreamWriter(outputStream), true);
+        printer.println("OFF");
         System.out.println("Shutting down system...");
     }
 
--- src/main/java/es/wei/Usuario.java
+++ src/main/java/es/wei/Usuario.java
@@ -1,44 +1,25 @@
 package es.wei;
 
+import es.florida.DronController;
+
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Usuario {
-    private static final int PORT_HTTP = 9876;
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket();
-        socket.connect(new InetSocketAddress("localhost", PORT_HTTP));
-        Thread ThreadComandos = new Thread(new Comandos(socket));
-        ThreadComandos.start();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+       DronController dronController=new DronController();
+       Socket usuario= dronController.connect();
+        dronController.takeOff(usuario);
+        Thread.sleep(1000);
+        dronController.firePrimaryCannon(usuario);
+        Thread.sleep(1000);
+        dronController.fireSecondaryWeapon(usuario);
+        dronController.land(usuario);
     }
 
-    private static class Comandos implements Runnable {
-        private Socket socket;
 
-        public Comandos(Socket socket) {
-            this.socket = socket;
-        }
 
-        public void run() {
-            OutputStream outputStream;
-            try {
-                outputStream = socket.getOutputStream();
-                PrintWriter printer = new PrintWriter(new OutputStreamWriter(outputStream), true);
-                printer.println("TAKE-OFF");
-                Thread.sleep(1000);
-                printer.println("FIRE_P_W");
-                Thread.sleep(1000);
-                printer.println("FIRE_S_W");
-                Thread.sleep(200);
-                printer.println("LAND");
-                printer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
